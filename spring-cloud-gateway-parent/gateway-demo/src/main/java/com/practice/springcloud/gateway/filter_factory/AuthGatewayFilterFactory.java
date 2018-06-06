2ae2855dd621208e9fe60a75260bd8ea27bc7269
luodb@ufotosoft.com <luobd@ufotosoft.com>
2018-06-06 19:58:05 +0800

    u

--- spring-cloud-gateway-parent/gateway-demo/src/main/java/com/practice/springcloud/gateway/filter_factory/AuthGatewayFilterFactory.java
+++ spring-cloud-gateway-parent/gateway-demo/src/main/java/com/practice/springcloud/gateway/filter_factory/AuthGatewayFilterFactory.java
@@ -2,8 +2,11 @@ package com.practice.springcloud.gateway.filter_factory;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.tuple.Tuple;
+
+import java.net.URI;
 
 /**
  * @author Luo Bao Ding
@@ -17,7 +20,29 @@ public class AuthGatewayFilterFactory implements GatewayFilterFactory {
 
         return (exchange, chain) -> {
 // ignore
-           return chain.filter(exchange);
+            return chain.filter(exchange);
         };
+    }
+
+    public GatewayFilter apply2(Tuple args) {
+        return (exchange, chain) ->
+                exchange.getFormData()
+                        .flatMap(formData -> {
+                            String username = formData.getFirst("username");
+                            String password = formData.getFirst("password");
+
+                            //...
+
+                            ServerHttpRequest modifiedRequest = exchange.getRequest().mutate()
+                                    .uri(URI.create("xx"))
+                                    .build();
+
+                            return chain.filter(
+                                    exchange
+                                            .mutate()
+                                            .request(modifiedRequest)
+                                            .build()
+                            );
+                        });
     }
 }
